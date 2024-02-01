05b207bff9d379b944e4ce37b6da6d4d7690e9c0
Dipak Salunkhe <97588015+dipak0789@users.noreply.github.com>
2024-02-01 21:52:46 +0530

    Add files via upload

---
+++ conditional/If_Else.java
@@ -0,0 +1,16 @@
+package com.conditional;
+
+public class If_Else {
+	public static void main (String args[]) {
+		int a = 10;
+		if(a!=10) {
+			System.out.println("done inside if");
+		}
+		else if(a<20) {
+			System.out.println("done inside else if");
+		}
+		else {
+			System.out.println("dione inside else");
+		}
+	}
+}
